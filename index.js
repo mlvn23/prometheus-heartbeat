bc13a0dc3a32feab3ec9ff738bfbf1a44b74570f
Melvin Lim <mlim@extrategic.com>
2022-09-24 16:46:06 -0700

    Add missing whitespace

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ server.use(function (err, req, res, next) {
   res.status(500).json({ success: false, error: err.message });
 });
 
-log('Server listening to' + process.env.PORT + ', metrics exposed on /metrics endpoint');
+log('Server listening to ' + process.env.PORT + ', metrics exposed on /metrics endpoint');
 log('Listening for heartbeats on /heartbeat/:name and authorizing with secret: ' + process.env.SECRET);
 server.listen(process.env.PORT);
